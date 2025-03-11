46e449c9d63c44c333465a986c654a11a85997dd
HerambInamke <heramb15012006@gmail.com>
2025-03-11 15:24:34 +0530

    Added form to create and list entities

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          purple: '#A729F5',
+          pink: '#FF47A1',
+          yellow: '#FFD700',
+        },
+        secondary: {
+          blue: '#00D4FF',
+          green: '#50C878',
+        },
+        neutral: {
+          white: '#F8F9FA',
+          charcoal: '#2B2B2B',
+        },
+      },
+    },
   },
   plugins: [],
 }
